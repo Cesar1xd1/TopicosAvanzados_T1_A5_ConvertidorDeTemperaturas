11537d434156753db8b8bc64aa39e56a8efccbde
Cesar Ramirez <cesar1xd1@gmail.com>
2021-02-19 16:06:01 -0600

    Se termino de programar el proyecto, ya con los eventos, se añadio por comodidad un boton de limpieza, solo vacia las cajas de texto, hice una operacion en el resultado para redondearlo y no tener numeros con decimales muy largos

--- src/PruebaConversionCalculadora.java
+++ src/PruebaConversionCalculadora.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.swing.*;
 
@@ -10,10 +12,11 @@ class ConvertidorTemperatura extends JFrame implements ActionListener{
 	JTextArea salida = new JTextArea();
 	JComboBox  enter = new JComboBox(); 
 	JComboBox out = new JComboBox();
+	JButton limpiar = new JButton("Limpiar");
 	
 	public ConvertidorTemperatura() {
 		getContentPane().setLayout(null);
-		setSize(320,130);
+		setSize(320,155);
 		getContentPane().setBackground(new Color(160,230,170));
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
@@ -43,7 +46,9 @@ class ConvertidorTemperatura extends JFrame implements ActionListener{
 		out.addItem("Fahrenheit");
 		out.addItem("Kelvin");
 		out.addItem("Rankine");
+		out.addActionListener(this);
 		add(out);
+		
 		
 		JLabel igual = new JLabel("=");
 		igual.setBounds(160, 50, 10, 20);
@@ -51,6 +56,25 @@ class ConvertidorTemperatura extends JFrame implements ActionListener{
 		
 		salida.setBounds(190, 50, 80, 20);
 		add(salida);
+		
+		
+		limpiar.setBounds(100, 85, 100, 20);
+		limpiar.addActionListener(this);
+		add(limpiar);
+	
+	
+		entrada.addKeyListener(new KeyAdapter() {
+            public void keyPressed(KeyEvent k) {
+                String digitado = entrada.getText();
+                int c =k.getKeyCode();
+                if ((k.getKeyChar() >= '0' && k.getKeyChar() <= '9')||(!digitado.contains(".")&&k.getKeyChar()=='.')||(c==KeyEvent.VK_BACK_SPACE)) {
+                    entrada.setEditable(true);
+                }else{
+                    entrada.setEditable(false);
+                }
+            }
+        });
+	
 	}
 	
 
@@ -59,6 +83,112 @@ class ConvertidorTemperatura extends JFrame implements ActionListener{
 	
 	
 	public void actionPerformed(ActionEvent e) {
+		double tempeEntrada = Double.parseDouble(entrada.getText());
+		double resultado = 0.0;
+		if(e.getSource()==limpiar) {
+			if(entrada.getText().length()!=0) {
+			entrada.setText("");
+			}
+			if(salida.getText().length()!=0) {
+				salida.setText("");
+				}
+		}
+		
+		if(e.getSource()==out) {
+			//CENTIGRADOS
+			if(enter.getSelectedIndex()==0) {
+				//Centigrados
+				if(out.getSelectedIndex()==0) {
+					resultado = tempeEntrada;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				//Farenheit
+				}else if(out.getSelectedIndex()==1) {
+					resultado = (tempeEntrada * (1.8)+32);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Kelvin
+				}else if(out.getSelectedIndex()==2) {
+					resultado = (tempeEntrada +273.15);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Rankine
+				}else if(out.getSelectedIndex()==3) {
+					resultado = (tempeEntrada*(1.8) +491.67);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				}
+					//FARENHEIT
+			}else if(enter.getSelectedIndex()==1) {
+				//Centigrados
+				if(out.getSelectedIndex()==0) {
+					resultado = (tempeEntrada-32) * (0.55);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				//Farenheit
+				}else if(out.getSelectedIndex()==1) {
+					resultado =tempeEntrada;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Kelvin
+				}else if(out.getSelectedIndex()==2) {
+					resultado = (tempeEntrada - 32)*(0.55)+273.15;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Rankine
+				}else if(out.getSelectedIndex()==3) {
+					resultado = (tempeEntrada+ 459.67);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				}
+				//KELIVN
+			}else if(enter.getSelectedIndex()==2) {
+				//Centigrados
+				if(out.getSelectedIndex()==0) {
+					resultado = (tempeEntrada - 273.15);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf((resultado)));
+				//Farenheit
+				}else if(out.getSelectedIndex()==1) {
+					resultado =(tempeEntrada - 273.15)*(1.8)+32;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Kelvin
+				}else if(out.getSelectedIndex()==2) {
+					resultado =tempeEntrada ;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Rankine
+				}else if(out.getSelectedIndex()==3) {
+					resultado = (tempeEntrada - 273.15)* 1.8 + 491.67;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				}
+				//RANKINE
+			}else if(enter.getSelectedIndex()==3) {
+				//Centigrados
+				if(out.getSelectedIndex()==0) {
+					resultado = (tempeEntrada - 491.67)*(0.55);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				//Farenheit
+				}else if(out.getSelectedIndex()==1) {
+					resultado =(tempeEntrada - 459.67);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Kelvin
+				}else if(out.getSelectedIndex()==2) {
+					resultado =tempeEntrada*(0.55) ;
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+					//Rankine
+				}else if(out.getSelectedIndex()==3) {
+					resultado = (tempeEntrada);
+					resultado = resultado/100.0*100.0;
+					salida.setText(String.valueOf(resultado));
+				}
+			}
+		}//IF MASTER
 		
 	}//Listener
 	
